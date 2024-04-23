6b37613ed868580945f82a9123927795668a28cc
Eugene Leonkov <e.leonkov@dental21.de>
2024-04-23 11:21:55 +0200

    upd

--- src/buff2steam.ts
+++ src/buff2steam.ts
@@ -1,7 +1,7 @@
 import { getBriefAsset, getGoodsSellOrder, getMarketGoods, postGoodsBuy } from './api/buff'
 import { getMarketPriceOverview } from './api/steam'
 import { MarketPriceOverview } from './types'
-import { sleep } from './utils'
+import { calculateROI, canMakePurchase, sleep } from './utils'
 
 const MARKET_CACHE: Record<string, MarketPriceOverview> = {}
 
@@ -32,51 +32,55 @@ export const buff2steam = async ({
       market_hash_name,
       goods_info: { steam_price },
     } of goods.data.items) {
-      const roi = ((+steam_price * 0.87) / +sell_min_price - 1) * 100
+      const sellMaxPrice = +steam_price
+      const sellMinPrice = +sell_min_price
 
-      if (roi < 50) continue
+      const initialRoi = calculateROI(sellMaxPrice, sellMinPrice)
+
+      if (initialRoi < 50) continue
 
       const cache = MARKET_CACHE[market_hash_name]
-      const overview = cache ?? (await getMarketPriceOverview({ market_hash_name }))
+      const marketOverview = cache ?? (await getMarketPriceOverview({ market_hash_name }))
 
-      console.log(`${market_hash_name}, ROI${roi.toFixed(2)}%`)
-      console.log('Market overview: ', JSON.stringify(overview))
-
-      if (!overview?.success || Number(overview.volume ?? '0') <= 50) {
-        if (overview?.success) MARKET_CACHE[market_hash_name] = overview
-
-        console.warn('Market price overview: ', JSON.stringify(overview))
+      if (!canMakePurchase({ marketOverview, sellMinPrice, minVolume: 50 })) {
+        console.log(
+          `Product ${market_hash_name} with initial ROI ${initialRoi.toFixed(2)}% and price ${sellMinPrice}$ has been skipped due to: ${JSON.stringify(marketOverview)}\n`
+        )
 
         break
       }
 
-      const {
-        data: { total_amount },
-      } = await getBriefAsset()
+      await logger({
+        message: `Product ${market_hash_name} with initial ROI ${initialRoi.toFixed(2)}% and price ${sellMinPrice}$ has been bought. Market overview: ${JSON.stringify(marketOverview)}`,
+      })
 
-      let totalAmount = Number(total_amount) ?? 0
+      // const {
+      //   data: { total_amount },
+      // } = await getBriefAsset()
 
-      const goods = await getGoodsSellOrder({ goods_id: id, max_price: sell_min_price, exclude_current_user: 1 })
+      // let totalAmount = Number(total_amount) ?? 0
 
-      for (const filteredGood of goods.data.items) {
-        if (Number(filteredGood.price) > totalAmount) {
-          await logger({ message: `No cash to buy "${market_hash_name}" for ${filteredGood.price}$`, error: true })
+      // const goods = await getGoodsSellOrder({ goods_id: id, max_price: sell_min_price, exclude_current_user: 1 })
 
-          break
-        }
+      // for (const filteredGood of goods.data.items) {
+      // if (Number(filteredGood.price) > totalAmount) {
+      //   await logger({ message: `No cash to buy "${market_hash_name}" for ${filteredGood.price}$`, error: true })
 
-        await postGoodsBuy({ sell_order_id: filteredGood.id, price: Number(filteredGood.price) })
-        await logger({ message: `Item "${market_hash_name}" has been bought! ROI: ${roi.toFixed(2)}` })
-        await sleep(3_000)
+      //   break
+      // }
 
-        totalAmount -= Number(filteredGood.price)
-      }
+      // await postGoodsBuy({ sell_order_id: filteredGood.id, price: Number(filteredGood.price) })
+      // await logger({ message: `Item "${market_hash_name}" has been bought! ROI: ${initialRoi.toFixed(2)}` })
+      // await sleep(3_000)
 
-      if (overview.success && overview.volume) {
-        MARKET_CACHE[market_hash_name] = overview
-      }
+      // totalAmount -= Number(filteredGood.price)
+      // }
 
-      await logger({ message: `Balance after transaction(s): ${totalAmount}$` })
+      MARKET_CACHE[market_hash_name] = marketOverview
+
+      await sleep(3_500)
+
+      // await logger({ message: `Balance after transaction(s): ${totalAmount}$` })
     }
 
     if (hasNextPage) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,34 @@
+import { MarketPriceOverview } from './types'
+
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms))
+
+export const stringToNumber = (str: string) => Number(str.replace(/[a-zA-Z$ ]/g, ''))
+
+export const calculateROI = (maxPrice: number, minPrice: number, fee: number = 13) => {
+  return ((maxPrice * 0.87) / minPrice - 1) * 100
+}
+
+export const canMakePurchase = ({
+  minVolume = 50,
+  sellMinPrice,
+  marketOverview,
+}: {
+  minVolume: number
+  sellMinPrice: number
+  marketOverview: MarketPriceOverview
+}) => {
+  if (
+    !marketOverview.success ||
+    !marketOverview.lowest_price ||
+    !marketOverview.median_price ||
+    !marketOverview.volume
+  ) {
+    return false
+  }
+
+  if (Number(marketOverview.volume) < minVolume) {
+    return false
+  }
+
+  return calculateROI(stringToNumber(marketOverview.median_price), sellMinPrice) >= 40
+}

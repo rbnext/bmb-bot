0614047c21cc93494bdd336d3e7d335c82f3d76b
Eugene Leonkov <eleonkov@gmail.com>
2024-09-06 09:41:56 +0200

    upd

--- src/api/telegram.ts
+++ src/api/telegram.ts
@@ -7,8 +7,8 @@ const http = axios.create({
 export const sendMessage = async (message: string): Promise<unknown> => {
   const { data } = await http.get(`/bot${process.env.BOT_TOKEN}/sendMessage`, {
     params: {
-      chat_id: process.env.TELEGRAM_CHAT_ID,
       parse_mode: 'HTML',
+      chat_id: process.env.TELEGRAM_CHAT_ID,
       text: message,
     },
   })

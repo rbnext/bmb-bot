279f45445a9dede953354891cba2905682b8253f
Eugene Leonkov <eleonkov@gmail.com>
2024-09-06 07:59:37 +0200

    upd

--- src/api/telegram.ts
+++ src/api/telegram.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-
 const http = axios.create({
   baseURL: 'https://api.telegram.org',
 })
--- src/buffDefault.ts
+++ src/buffDefault.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import { differenceInDays , format } from 'date-fns'
+import { differenceInDays, format } from 'date-fns'
 import { getMarketGoods, getMarketGoodsBillOrder } from './api/buff'
 import { median, sleep } from './utils'
 import { sendMessage } from './api/telegram'
@@ -8,7 +8,7 @@ import { sendMessage } from './api/telegram'
 let lastMarketHashName: string | null = null
 
 const buffDefault = async () => {
-// await sendMessage(`🤖 I am working!`)
+  // await sendMessage(`🤖 I am working!`)
   try {
     const marketGoods = await getMarketGoods({ min_price: 5, max_price: 100 })
     const now = format(new Date(), 'dd MMM yyyy, HH:mm')
@@ -38,12 +38,17 @@ const buffDefault = async () => {
           const sales = salesLastWeek.map(({ price }) => Number(price))
           const median_price = median(sales.filter((price) => current_price * 2 > price))
           const estimated_profit = ((median_price * 0.975) / current_price - 1) * 100
+
+          console.log(`${now}: ${item.market_hash_name} ${item.sell_min_price}$`)
+
           if (estimated_profit > 4) {
-            await sendMessage(`🤖 *bold MAIN PAGE BOT\n${now}\n\n` + 
-            `${item.market_hash_name}.\n` +
-            `Buff price: ${current_price}$\n` +
-            `Estimated profit: ${estimated_profit.toFixed(2)}% if sale for ${median_price}$\n` +
-            `Buff market link: https://buff.market/market/goods/${goods_id}`)
+            await sendMessage(
+              `🤖 *bold MAIN PAGE BOT\n${now}\n\n` +
+                `${item.market_hash_name}.\n` +
+                `Buff price: ${current_price}$\n` +
+                `Estimated profit: ${estimated_profit.toFixed(2)}% if sale for ${median_price}$\n` +
+                `Buff market link: https://buff.market/market/goods/${goods_id}`
+            )
           }
 
           await sleep(1_000)
@@ -53,6 +58,8 @@ const buffDefault = async () => {
       lastMarketHashName = items[0].market_hash_name
     }
   } catch (error) {
+    console.log(error)
+
     return
   }
 

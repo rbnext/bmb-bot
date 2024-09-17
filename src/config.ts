4dffcffc9dd0531cc3eae1d8546021ed784e7efc
Eugene Leonkov <eleonkov@gmail.com>
2024-09-17 16:16:35 +0200

    UPF

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const weaponGroups = ['knife', 'rifle', 'pistol', 'smg', 'shotgun', 'machinegun', 'type_customplayer'] //sticker,type_customplayer,other
 
-export const REFERENCE_DIFF_THRESHOLD = 5
+export const REFERENCE_DIFF_THRESHOLD = 10
 export const STEAM_PURCHASE_THRESHOLD = 70
 export const STEAM_CHECK_THRESHOLD = 55

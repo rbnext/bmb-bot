cf564543cba1b2394b477ec3e6b2362e558cd6fe
Eugene <eleonkov@gmail.com>
2024-09-28 09:30:56 +0200

    upd

--- src/bots/buffDefault.ts
+++ src/bots/buffDefault.ts
@@ -23,7 +23,7 @@ const buffDefault = async () => {
       const goods_id = item.id
       const current_price = Number(item.sell_min_price)
 
-      if (goods_id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[goods_id].price, current_price, 0.05)) {
+      if (goods_id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[goods_id].price, current_price, 0.1)) {
         GOODS_CACHE[goods_id].price = current_price
 
         continue

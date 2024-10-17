47eba7b8f13976bfbcac2a4c155c31a2aa030a05
Eugene <eleonkov@gmail.com>
2024-10-17 19:57:24 +0200

    update

--- src/helpers/executeBuffToSteamTrade.ts
+++ src/helpers/executeBuffToSteamTrade.ts
@@ -1,10 +1,10 @@
 import { format } from 'date-fns'
-import { getGoodsSellOrder } from '../api/buff'
+import { getGoodsInfo, getGoodsSellOrder, getMarketGoodsBillOrder } from '../api/buff'
 import { sendMessage } from '../api/telegram'
 import { createVercelPurchase } from '../api/vercel'
 import { STEAM_CHECK_THRESHOLD, STEAM_PURCHASE_THRESHOLD } from '../config'
 import { MarketGoodsItem, MessageType, Source } from '../types'
-import { generateMessage } from '../utils'
+import { generateMessage, median, sleep } from '../utils'
 import { getMaxPricesForXDays } from './getMaxPricesForXDays'
 
 export const executeBuffToSteamTrade = async (
@@ -30,8 +30,10 @@ export const executeBuffToSteamTrade = async (
   const min_steam_price = prices.length !== 0 ? Math.min(...prices) : 0
   const estimated_profit = ((min_steam_price - current_price) / current_price) * 100
 
-  console.log(`${now}: ${item.market_hash_name} min steam price `, Number(min_steam_price.toFixed(2)))
-  console.log(`${now}: ${item.market_hash_name} estimated profit `, Number(estimated_profit.toFixed(2)))
+  console.log('***')
+  console.log(`${now}: ${item.market_hash_name} min steam price $${min_steam_price.toFixed(2)}`)
+  console.log(`${now}: ${item.market_hash_name} estimated profit ${estimated_profit.toFixed(2)}%`)
+  console.log('***')
 
   if (estimated_profit >= STEAM_PURCHASE_THRESHOLD) {
     const orders = await getGoodsSellOrder({ goods_id, exclude_current_user: 1 })
@@ -66,6 +68,55 @@ export const executeBuffToSteamTrade = async (
         medianPrice: min_steam_price,
         float: lowestPricedItem.asset_info.paintwear,
         source: options.source,
+      })
+    )
+  } else if (estimated_profit >= STEAM_PURCHASE_THRESHOLD - 10 && current_price >= 5) {
+    const orders = await getGoodsSellOrder({ goods_id, exclude_current_user: 1 })
+
+    const lowestPricedItem = orders.data.items.find((el) => el.price === item.sell_min_price)
+
+    if (!lowestPricedItem) {
+      await sendMessage(`[${options.source}] Someone already bought the ${item.market_hash_name} item.`)
+
+      return
+    }
+
+    const response = await createVercelPurchase({ price: current_price, sell_order_id: lowestPricedItem.id })
+
+    if (response.code !== 'OK') {
+      await sendMessage(
+        `[${options.source}] Failed to purchase the item ${item.market_hash_name}. Reason: ${response.code}`
+      )
+
+      return
+    }
+
+    await sleep(3_000)
+
+    const goods_info = await getGoodsInfo({ goods_id })
+    const history = await getMarketGoodsBillOrder({ goods_id })
+
+    const sales = history.data.items.map(({ price }) => Number(price))
+    const median_price = median(sales.filter((price) => current_price * 2 > price))
+    const estimated_profit = ((median_price * 0.975) / current_price - 1) * 100
+
+    const goods_ref_price = Number(goods_info.data.goods_info.goods_ref_price)
+    const ref_price_delta = (goods_ref_price / current_price - 1) * 100
+
+    await sendMessage(
+      generateMessage({
+        id: goods_id,
+        price: current_price,
+        type: MessageType.Purchased,
+        source: Source.BUFF_DEFAULT,
+        name: item.market_hash_name,
+        createdAt: lowestPricedItem.created_at,
+        updatedAt: lowestPricedItem.updated_at,
+        estimatedProfit: estimated_profit,
+        medianPrice: median_price,
+        float: lowestPricedItem.asset_info.paintwear,
+        referencePrice: goods_ref_price,
+        refPriceDelta: ref_price_delta,
       })
     )
   }

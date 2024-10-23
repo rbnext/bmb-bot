fe892406634c860c89312186e317f81aefb5533d
Eugene <eleonkov@gmail.com>
2024-10-23 14:43:55 +0200

    test

--- src/helpers/executeBuffToSteamTrade.ts
+++ src/helpers/executeBuffToSteamTrade.ts
@@ -1,10 +1,9 @@
 import { format } from 'date-fns'
-import { getGoodsInfo, getGoodsSellOrder, getMarketGoodsBillOrder } from '../api/buff'
+import { getGoodsSellOrder, postGoodsBuy } from '../api/buff'
 import { sendMessage } from '../api/telegram'
-import { createVercelPurchase, vercelHealthCheck } from '../api/vercel'
 import { STEAM_CHECK_THRESHOLD, STEAM_PURCHASE_THRESHOLD } from '../config'
 import { MarketGoodsItem, MessageType, Source } from '../types'
-import { generateMessage, median, sleep } from '../utils'
+import { generateMessage } from '../utils'
 import { getMaxPricesForXDays } from './getMaxPricesForXDays'
 
 export const executeBuffToSteamTrade = async (
@@ -13,7 +12,7 @@ export const executeBuffToSteamTrade = async (
     source: Source
   }
 ) => {
-  vercelHealthCheck()
+  // vercelHealthCheck()
 
   const goods_id = item.id
   const current_price = Number(item.sell_min_price)
@@ -53,7 +52,7 @@ export const executeBuffToSteamTrade = async (
       return
     }
 
-    const response = await createVercelPurchase({ price: current_price, sell_order_id: lowestPricedItem.id })
+    const response = await postGoodsBuy({ price: current_price, sell_order_id: lowestPricedItem.id })
 
     if (response.code !== 'OK') {
       await sendMessage(
@@ -63,7 +62,7 @@ export const executeBuffToSteamTrade = async (
       return
     }
 
-    await sendMessage(
+    sendMessage(
       generateMessage({
         id: goods_id,
         price: current_price,
@@ -77,56 +76,58 @@ export const executeBuffToSteamTrade = async (
         source: options.source,
       })
     )
-  } else if (current_price >= 5 && estimated_profit >= STEAM_PURCHASE_THRESHOLD - 8) {
-    const orders = await getGoodsSellOrder({ goods_id, exclude_current_user: 1 })
-
-    const lowestPricedItem = orders.data.items.find((el) => el.price === item.sell_min_price)
-
-    if (!lowestPricedItem) {
-      await sendMessage(
-        `[${Source.BUFF_DEFAULT}] Someone already bought the ${item.market_hash_name} item for $${current_price}.`
-      )
-
-      return
-    }
-
-    const response = await createVercelPurchase({ price: current_price, sell_order_id: lowestPricedItem.id })
-
-    if (response.code !== 'OK') {
-      await sendMessage(
-        `[${Source.BUFF_DEFAULT}] Failed to purchase the item ${item.market_hash_name}. Reason: ${response.code}`
-      )
-
-      return
-    }
-
-    await sleep(3_000)
-
-    const goods_info = await getGoodsInfo({ goods_id })
-    const history = await getMarketGoodsBillOrder({ goods_id })
-
-    const sales = history.data.items.map(({ price }) => Number(price))
-    const median_price = median(sales.filter((price) => current_price * 2 > price))
-    const estimated_profit = ((median_price * 0.975) / current_price - 1) * 100
-
-    const goods_ref_price = Number(goods_info.data.goods_info.goods_ref_price)
-    const ref_price_delta = (goods_ref_price / current_price - 1) * 100
-
-    await sendMessage(
-      generateMessage({
-        id: goods_id,
-        price: current_price,
-        type: MessageType.Purchased,
-        source: Source.BUFF_DEFAULT,
-        name: item.market_hash_name,
-        createdAt: lowestPricedItem.created_at,
-        updatedAt: lowestPricedItem.updated_at,
-        estimatedProfit: estimated_profit,
-        medianPrice: median_price,
-        float: lowestPricedItem.asset_info.paintwear,
-        referencePrice: goods_ref_price,
-        refPriceDelta: ref_price_delta,
-      })
-    )
   }
+
+  // else if (current_price >= 5 && estimated_profit >= STEAM_PURCHASE_THRESHOLD - 8) {
+  //   const orders = await getGoodsSellOrder({ goods_id, exclude_current_user: 1 })
+
+  //   const lowestPricedItem = orders.data.items.find((el) => el.price === item.sell_min_price)
+
+  //   if (!lowestPricedItem) {
+  //     await sendMessage(
+  //       `[${Source.BUFF_DEFAULT}] Someone already bought the ${item.market_hash_name} item for $${current_price}.`
+  //     )
+
+  //     return
+  //   }
+
+  //   const response = await createVercelPurchase({ price: current_price, sell_order_id: lowestPricedItem.id })
+
+  //   if (response.code !== 'OK') {
+  //     await sendMessage(
+  //       `[${Source.BUFF_DEFAULT}] Failed to purchase the item ${item.market_hash_name}. Reason: ${response.code}`
+  //     )
+
+  //     return
+  //   }
+
+  //   await sleep(3_000)
+
+  //   const goods_info = await getGoodsInfo({ goods_id })
+  //   const history = await getMarketGoodsBillOrder({ goods_id })
+
+  //   const sales = history.data.items.map(({ price }) => Number(price))
+  //   const median_price = median(sales.filter((price) => current_price * 2 > price))
+  //   const estimated_profit = ((median_price * 0.975) / current_price - 1) * 100
+
+  //   const goods_ref_price = Number(goods_info.data.goods_info.goods_ref_price)
+  //   const ref_price_delta = (goods_ref_price / current_price - 1) * 100
+
+  //   await sendMessage(
+  //     generateMessage({
+  //       id: goods_id,
+  //       price: current_price,
+  //       type: MessageType.Purchased,
+  //       source: Source.BUFF_DEFAULT,
+  //       name: item.market_hash_name,
+  //       createdAt: lowestPricedItem.created_at,
+  //       updatedAt: lowestPricedItem.updated_at,
+  //       estimatedProfit: estimated_profit,
+  //       medianPrice: median_price,
+  //       float: lowestPricedItem.asset_info.paintwear,
+  //       referencePrice: goods_ref_price,
+  //       refPriceDelta: ref_price_delta,
+  //     })
+  //   )
+  // }
 }

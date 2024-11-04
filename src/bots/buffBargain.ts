ebb13dae4f4d97acc633f2680d3ed013f19d6180
Eugene <eleonkov@gmail.com>
2024-11-04 15:28:51 +0100

    update

--- src/bots/buffBargain.ts
+++ src/bots/buffBargain.ts
@@ -14,7 +14,7 @@ export const GOODS_BLACKLIST_CACHE: number[] = []
 
 const buffBargain = async () => {
   try {
-    const marketGoods = await getMarketGoods({ min_price: 5, max_price: 100 })
+    const marketGoods = await getMarketGoods({ min_price: 5, max_price: 16.84 })
 
     for (const item of marketGoods.data.items) {
       const now = format(new Date(), 'HH:mm:ss')
@@ -64,7 +64,7 @@ const buffBargain = async () => {
   const pages = Array.from({ length: 50 }, (_, i) => i + 1)
 
   for (const page_num of pages) {
-    const goods = await getMarketGoods({ page_num, min_price: 10, max_price: 100 })
+    const goods = await getMarketGoods({ page_num, min_price: 5, max_price: 100 })
     for (const item of goods.data.items) GOODS_CACHE[item.id] = { price: Number(item.sell_min_price) }
     if (goods.data.items.length !== 50) break
     await sleep(5_000)

ae0d5abecf010f54567ac2107a8298e24f468475
Eugene <eleonkov@gmail.com>
2024-11-18 15:52:23 +0100

    UPDATE

--- src/bots/steamMarketChecker.ts
+++ src/bots/steamMarketChecker.ts
@@ -26,7 +26,7 @@ const findSteamItemInfo = async (market_hash_name: string) => {
 
   try {
     const isCharm = market_hash_name.includes('Charm')
-    const steam = await getMarketRender({ market_hash_name, count: isCharm ? 100 : 20 })
+    const steam = await getMarketRender({ market_hash_name, count: isCharm ? 50 : 20 })
 
     for (const [index, listingId] of Object.keys(steam.listinginfo).entries()) {
       if (CASHED_LISTINGS.has(listingId)) continue

ec043b49452f43ed6c1425ce0ccf5244060c236e
Eugene <eleonkov@gmail.com>
2024-11-24 18:22:53 +0100

    asd

--- src/bots/steamMarketChecker.ts
+++ src/bots/steamMarketChecker.ts
@@ -105,7 +105,7 @@ const findSteamItemInfo = async (page: Page, market_hash_name: string) => {
 
 ;(async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     executablePath: 'C:\\Users\\Administrator\\Downloads\\Win_x64_1067886_chrome-win\\chrome-win\\chrome.exe',
     pipe: true,
   })

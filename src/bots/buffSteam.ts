8084cbe9fa317d132e1d256c9bb2330796f38037
Eugene <eleonkov@gmail.com>
2024-11-26 19:55:26 +0100

    update

--- src/bots/buffSteam.ts
+++ src/bots/buffSteam.ts
@@ -65,10 +65,10 @@ const buffSteam = async () => {
 }
 
 ;(async () => {
-  const pages = Array.from({ length: 50 }, (_, i) => i + 1)
+  const pages = Array.from({ length: 80 }, (_, i) => i + 1)
 
   for (const page_num of pages) {
-    const goods = await getMarketGoods({ page_num, min_price: 1, max_price: 100 })
+    const goods = await getMarketGoods({ page_num, min_price: 1, max_price: 150 })
     for (const item of goods.data.items) GOODS_CACHE[item.id] = { price: Number(item.sell_min_price) }
     if (goods.data.items.length !== 50) break
     await sleep(5_000)

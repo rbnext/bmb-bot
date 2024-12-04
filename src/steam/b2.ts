b3811e65b8a74340c6bf4a7aec4a485af84e611d
Eugene <eleonkov@gmail.com>
2024-12-04 12:58:32 +0100

    update

--- src/steam/b2.ts
+++ src/steam/b2.ts
@@ -10,46 +10,66 @@ import Bottleneck from 'bottleneck'
 const CONFIG = [
   {
     market_hash_names: [
-      'P90 | Asiimov (Field-Tested)',
-      'Desert Eagle | Mecha Industries (Minimal Wear)',
-      'AWP | Chromatic Aberration (Field-Tested)',
-      'Desert Eagle | Conspiracy (Minimal Wear)',
+      // 'P90 | Asiimov (Field-Tested)',
+      // 'Desert Eagle | Mecha Industries (Minimal Wear)',
+      // 'AWP | Chromatic Aberration (Field-Tested)',
+      // 'Desert Eagle | Conspiracy (Minimal Wear)',
+      'AK-47 | Red Laminate (Well-Worn)',
+      "M4A1-S | Chantico's Fire (Minimal Wear)",
+      'AK-47 | Wasteland Rebel (Field-Tested)',
+      'USP-S | Orion (Minimal Wear)',
     ],
     proxy: 'http://S0oH5AJkr2:hCdq69iwKV@185.156.75.160:15017', // ru
   },
   {
     market_hash_names: [
-      // 'Desert Eagle | Heat Treated (Minimal Wear)',
-      'M4A4 | 龍王 (Dragon King) (Field-Tested)',
-      'M4A4 | 龍王 (Dragon King) (Minimal Wear)',
-      'M4A4 | X-Ray (Minimal Wear)',
+      // 'Desert Eagle | Heat Treated (Minimal Wear)', something wrong with this item!
+      // 'M4A4 | 龍王 (Dragon King) (Field-Tested)',
+      // 'M4A4 | 龍王 (Dragon King) (Minimal Wear)',
+      // 'M4A4 | X-Ray (Minimal Wear)',
+      'AK-47 | Bloodsport (Field-Tested)',
+      'AK-47 | Wasteland Rebel (Field-Tested)',
+      'M4A1-S | Golden Coil (Minimal Wear)',
+      'AWP | Chrome Cannon (Factory New)',
     ],
     proxy: 'http://TNJr9sjaxY:wJcQyV6T1o@185.156.75.126:13350', // new ru
   },
   {
     market_hash_names: [
-      'AK-47 | Phantom Disruptor (Factory New)',
-      'USP-S | The Traitor (Field-Tested)',
-      'AWP | Chromatic Aberration (Minimal Wear)',
-      'AWP | Atheris (Factory New)',
+      // 'AK-47 | Phantom Disruptor (Factory New)',
+      // 'USP-S | The Traitor (Field-Tested)',
+      // 'AWP | Chromatic Aberration (Minimal Wear)',
+      // 'AWP | Atheris (Factory New)',
+      'AK-47 | Fuel Injector (Well-Worn)',
+      'AK-47 | Frontside Misty (Factory New)',
+      'AK-47 | The Empress (Factory New)',
+      'StatTrak™ AK-47 | Point Disarray (Factory New)',
     ],
     proxy: 'http://L5B6t89Uef:EhqDzFUpJi@194.63.143.13:14362', // new ru
   },
   {
     market_hash_names: [
-      'AWP | Fever Dream (Field-Tested)',
-      'M4A4 | The Emperor (Field-Tested)',
-      'M4A4 | Neo-Noir (Minimal Wear)',
-      'M4A1-S | Decimator (Field-Tested)',
+      // 'AWP | Fever Dream (Field-Tested)',
+      // 'M4A4 | The Emperor (Field-Tested)',
+      // 'M4A4 | Neo-Noir (Minimal Wear)',
+      // 'M4A1-S | Decimator (Field-Tested)',
+      'StatTrak™ AK-47 | Point Disarray (Field-Tested)',
+      'StatTrak™ M4A1-S | Black Lotus (Factory New)',
+      'AK-47 | Aquamarine Revenge (Factory New)',
+      'StatTrak™ AWP | Neo-Noir (Minimal Wear)',
     ],
     proxy: 'http://zhDFociVpC:4cjHL08TwR@185.5.251.252:14016', // new ru
   },
   {
     market_hash_names: [
-      'AK-47 | Blue Laminate (Factory New)',
-      'M4A4 | Buzz Kill (Minimal Wear)',
-      'M4A1-S | Basilisk (Factory New)',
-      'Glock-18 | Water Elemental (Factory New)',
+      // 'AK-47 | Blue Laminate (Factory New)',
+      // 'M4A4 | Buzz Kill (Minimal Wear)',
+      // 'M4A1-S | Basilisk (Factory New)',
+      // 'Glock-18 | Water Elemental (Factory New)',
+      'AWP | Sun in Leo (Minimal Wear)',
+      'Desert Eagle | Golden Koi (Factory New)',
+      'AK-47 | Vulcan (Field-Tested)',
+      'M4A1-S | Mecha Industries (Minimal Wear)',
     ],
     proxy: 'http://v1NLVgpJBt:axmFWc47sd@185.5.250.21:13626', // new ru
   },

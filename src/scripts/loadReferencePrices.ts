b0402e6b60f3c076c36601a2e2cc0f66245a8486
Eugene <eleonkov@gmail.com>
2024-12-06 11:54:02 +0100

    updtae

--- src/scripts/loadReferencePrices.ts
+++ src/scripts/loadReferencePrices.ts
@@ -1,44 +1,46 @@
-// import 'dotenv/config'
+import 'dotenv/config'
 
-// import { readFileSync, writeFileSync } from 'fs'
-// import { getGoodsInfo, getMarketGoods } from '../api/buff'
-// import path from 'path'
-// import { sleep } from '../utils'
-// import { SteamDBItem } from '../types'
-// ;(async () => {
-//   const pathname = path.join(__dirname, '../steam/goods.json')
+import { readFileSync, writeFileSync } from 'fs'
+import { getGoodsInfo, getMarketGoods } from '../api/buff'
+import path from 'path'
+import { sleep } from '../utils'
+import { SteamDBItem } from '../types'
+;(async () => {
+  const pages = Array.from({ length: 100 }, (_, i) => i + 1)
+  const pathname = path.join(__dirname, '../../buff.json')
 
-//   const items: SteamDBItem = JSON.parse(readFileSync(pathname, 'utf8'))
+  for (const page_num of pages) {
+    console.log([page_num])
 
-//   const sortedEntries = Object.entries(items).sort(([, a], [, b]) => a.reference_price - b.reference_price)
+    const goods = await getMarketGoods({
+      page_num,
+      category_group: 'sticker',
+    })
+    for (const item of goods.data.items) {
+      const data: SteamDBItem = JSON.parse(readFileSync(pathname, 'utf8'))
 
-//   const sortedItems = Object.fromEntries(sortedEntries)
+      if (!data[item.market_hash_name] && item.id) {
+        const goodsInfo = await getGoodsInfo({ goods_id: item.id })
+        const reference_price = Number(goodsInfo.data.goods_info.goods_ref_price)
 
-//   writeFileSync(pathname, JSON.stringify(sortedItems, null, 4))
+        console.log(item.market_hash_name, reference_price)
 
-//   // for (const name of Object.keys(items)) {
-//   //   const goods = await getMarketGoods({ search: name })
-//   //   const goods_id = goods.data.items.find((el) => el.market_hash_name === name)?.id
+        writeFileSync(
+          pathname,
+          JSON.stringify(
+            {
+              ...data,
+              [item.market_hash_name]: { goods_id: item.id, reference_price },
+            },
+            null,
+            4
+          )
+        )
 
-//   //   if (goods_id) {
-//   //     const goodsInfo = await getGoodsInfo({ goods_id })
-//   //     const referencePrice = Number(goodsInfo.data.goods_info.goods_ref_price)
+        await sleep(3_000)
+      }
+    }
 
-//   //     const data: SteamDBItem = JSON.parse(readFileSync(pathname, 'utf8'))
-
-//   //     writeFileSync(
-//   //       pathname,
-//   //       JSON.stringify(
-//   //         {
-//   //           ...data,
-//   //           [name]: { goods_id, reference_price: referencePrice },
-//   //         },
-//   //         null,
-//   //         4
-//   //       )
-//   //     )
-//   //   }
-
-//   //   await sleep(5_000)
-//   // }
-// })()
+    await sleep(3_000)
+  }
+})()

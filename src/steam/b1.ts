594f4f793a529f17879a77957cdd4d4beca93a17
Eugene <eleonkov@gmail.com>
2024-12-07 23:18:44 +0100

    update

--- src/steam/b1.ts
+++ src/steam/b1.ts
@@ -156,7 +156,6 @@ const fetchSteamMarketItem = async (config: { market_hash_name: string; proxy: s
     if (marketHashNameData) marketHashNameData.steamDataFetched = true
     if (marketHashNameData) marketHashNameData.lastRequested = Date.now()
   } catch (error) {
-    console.log(error)
     console.log(format(new Date(), 'HH:mm:ss'), config.proxy, error.message)
 
     if (proxyData && error.message.includes('429')) {

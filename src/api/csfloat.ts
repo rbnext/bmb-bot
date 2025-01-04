2b34adf33f66c1a9b6ec1bdafaceb8967453f083
Eugene <eleonkov@gmail.com>
2025-01-04 20:12:49 +0100

    update

--- src/api/csfloat.ts
+++ src/api/csfloat.ts
@@ -34,8 +34,7 @@ export const getCSFloatListings = async ({
   const { data } = await http.get('/v1/listings', {
     params: { limit, category, type, min_float, max_float, market_hash_name, sort_by, min_price, max_price },
     headers: {
-      Cookie:
-        'session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGVhbV9pZCI6Ijc2NTYxMTk5NjI1NTU0OTc1Iiwibm9uY2UiOjAsImltcGVyc29uYXRlZCI6ZmFsc2UsImlzcyI6ImNzdGVjaCIsImV4cCI6MTczNjAwMDI1MH0.s4KtsvDD5mUKlxgo0X6Zs5dfz5RBXQv5V9oNnnTSZRQ',
+      Cookie: `session=${process.env.CSFLOAT_SESSION_TOKEN}`,
     },
   })
 
@@ -46,8 +45,7 @@ export const getBuyOrders = async ({ id, limit = 10 }: { id: string; limit?: num
   const { data, headers } = await http.get(`/v1/listings/${id}/buy-orders`, {
     params: { limit },
     headers: {
-      Cookie:
-        'session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGVhbV9pZCI6Ijc2NTYxMTk5NjI1NTU0OTc1Iiwibm9uY2UiOjAsImltcGVyc29uYXRlZCI6ZmFsc2UsImlzcyI6ImNzdGVjaCIsImV4cCI6MTczNjAwMDI1MH0.s4KtsvDD5mUKlxgo0X6Zs5dfz5RBXQv5V9oNnnTSZRQ',
+      Cookie: `session=${process.env.CSFLOAT_SESSION_TOKEN}`,
     },
   })
 

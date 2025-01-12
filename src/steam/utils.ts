5ed56a564f70491cf0b75c90d59cff41680e52c9
Eugene <eleonkov@gmail.com>
2025-01-12 21:09:01 +0100

    remove old code

--- src/steam/utils.ts
+++ src/steam/utils.ts
@@ -2,22 +2,6 @@ export const getInspectLink = (link: string, assetId: string, listingId: string)
   return link.replace('%assetid%', assetId).replace('%listingid%', listingId)
 }
 
-export const calculateTotalCost = (stickers: string[], details: Record<string, number>): number => {
-  const groupByStickerName = stickers.reduce<Record<string, number>>((acc, name) => {
-    return { ...acc, [name]: (acc[name] || 0) + 1 }
-  }, {})
-
-  const totalCost = Object.keys(groupByStickerName).reduce((acc, name) => {
-    const price = details[name] || 0
-    const stickerCount = groupByStickerName[name]
-    const discountRate = stickerCount >= 4 ? 0.4 : 0.15
-
-    return acc + price * discountRate * stickerCount
-  }, 0)
-
-  return totalCost
-}
-
 export const isStickerCombo = (stickers: string[]) => {
   const groupByStickerName = stickers.reduce<Record<string, number>>((acc, name) => {
     return { ...acc, [name]: (acc[name] || 0) + 1 }

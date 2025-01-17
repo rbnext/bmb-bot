4fee2c2162b8c9b7ef0280ef9cdcf2434eb154a7
Eugene <eleonkov@gmail.com>
2025-01-17 10:24:40 +0100

    update

--- src/steam/steam.v4.ts
+++ src/steam/steam.v4.ts
@@ -80,7 +80,7 @@ const findSteamItemInfo = async ({ market_hash_name, proxy }: { market_hash_name
           `|___ ST: $${stickerTotal.toFixed(2)}; SP: ${SP.toFixed(2)}%; Combo: ${String(isStickerCombo(stickers))}`
         )
 
-        if (SP < (isStickerCombo(stickers) ? 18 : 8)) {
+        if (SP < (isStickerCombo(stickers) ? 25 : 10)) {
           const itemInfoResponse = await getCSFloatItemInfo({ url: inspectLink })
 
           const message: string[] = []
@@ -102,29 +102,29 @@ const findSteamItemInfo = async ({ market_hash_name, proxy }: { market_hash_name
           message.push(`<b>Stickers total</b>: $${stickerTotal.toFixed(2)}\n\n`)
           message.push(`<b>Float</b>: ${itemInfoResponse.iteminfo.floatvalue}\n\n`)
 
-          const sentMessage = await sendMessage(message.join(''))
+          await sendMessage(message.join(''))
 
-          if (price && price <= 20 && (itemInfoResponse.iteminfo.stickers || [])?.every((item) => item.wear === 0)) {
-            try {
-              const response = await stemMarketBuyListing({
-                idListing: currentListing.listingid,
-                market_hash_name: market_hash_name,
-                converted_price: currentListing.converted_price,
-                converted_fee: currentListing.converted_fee,
-              })
+          // if (price && price <= 20 && (itemInfoResponse.iteminfo.stickers || [])?.every((item) => item.wear === 0)) {
+          //   try {
+          //     const response = await stemMarketBuyListing({
+          //       idListing: currentListing.listingid,
+          //       market_hash_name: market_hash_name,
+          //       converted_price: currentListing.converted_price,
+          //       converted_fee: currentListing.converted_fee,
+          //     })
 
-              if (response?.wallet_info?.success === 1) {
-                await sendMessage('Success purchase', sentMessage.result.message_id)
-              } else {
-                await sendMessage('Failed purchase', sentMessage.result.message_id)
-              }
+          //     if (response?.wallet_info?.success === 1) {
+          //       await sendMessage('Success purchase', sentMessage.result.message_id)
+          //     } else {
+          //       await sendMessage('Failed purchase', sentMessage.result.message_id)
+          //     }
 
-              console.log(response)
-            } catch (error) {
-              console.log(error)
-              await sendMessage(`Steam failed to purchase the ${market_hash_name} item.`)
-            }
-          }
+          //     console.log(response)
+          //   } catch (error) {
+          //     console.log(error)
+          //     await sendMessage(`Steam failed to purchase the ${market_hash_name} item.`)
+          //   }
+          // }
         }
 
         await sleep(3_000)

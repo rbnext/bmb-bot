1758de5f5458c4168835feb3181bb5e78d92950d
Eugene <eleonkov@gmail.com>
2025-01-17 15:18:59 +0100

    steam float

--- src/steam/config.ts
+++ src/steam/config.ts
@@ -2,4 +2,5 @@ export const MARKET_BLACK_LIST = [
   'AK-47 | Redline (Field-Tested)',
   'AK-47 | Slate (Field-Tested)',
   'AK-47 | Ice Coaled (Field-Tested)',
+  'Desert Eagle | Heat Treated (Factory New)',
 ]

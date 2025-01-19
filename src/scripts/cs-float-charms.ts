a59853c7429a421fb12109061b62fac5abf74d00
Eugene <eleonkov@gmail.com>
2025-01-19 14:10:03 +0100

    update

--- src/scripts/cs-float-charms.ts
+++ src/scripts/cs-float-charms.ts
@@ -3,8 +3,21 @@ import 'dotenv/config'
 import { sleep } from '../utils'
 import { getCSFloatListings } from '../api/csfloat'
 import { sendMessage } from '../api/telegram'
+import { format } from 'date-fns'
 
 const CASHED_LISTINGS = new Set<string>()
+
+const getPredictedPrice = (pattern: number, base: number) => {
+  if (pattern >= 1 && pattern <= 5000) return 15
+  if (pattern > 5000 && pattern <= 9000) return 10
+  if (pattern >= 20000 && pattern <= 23000) return 17
+  if (pattern > 23000 && pattern <= 25000) return 10
+  if (pattern >= 90000 && pattern <= 94999) return 8
+  if (pattern >= 95000 && pattern <= 98999) return 12
+  if (pattern >= 99000 && pattern <= 99999) return 20
+
+  return base
+}
 
 const csFloatCharms = async () => {
   const response = await getCSFloatListings({ keychains: '[{"i":18}]' })
@@ -14,27 +27,32 @@ const csFloatCharms = async () => {
 
     const keychains = data.item.keychains ?? []
     const currentPrice = Number((data.price / 100).toFixed(2))
-    const basePrice = Number((data.reference.base_price / 100).toFixed(2))
-    const keychainTotal = keychains.reduce((acc, cur) => acc + cur.reference.price, 0) / 100
+    const predictedPrice = Number((data.reference.predicted_price / 100).toFixed(2))
+    const keychain = keychains.find((k) => k.name === 'Charm | Die-cast AK')
 
-    console.log(data.item.market_hash_name)
-    for (const keychain of keychains) console.log(`|___ ${keychain.name}: ${keychain.pattern}`)
+    if (keychain) {
+      const now = format(new Date(), 'HH:mm:ss')
+      const keychainPrice = getPredictedPrice(keychain.pattern, keychain.reference.price / 100)
+      const profit = currentPrice - predictedPrice - keychainPrice - 0.33
 
-    if (basePrice + keychainTotal > currentPrice) {
-      const message: string[] = []
+      console.log(now, data.item.market_hash_name, profit.toFixed(2))
 
-      message.push(`<a href="https://csfloat.com/item/${data.id}">${data.item.market_hash_name}</a>\n\n`)
+      if (profit > 0) {
+        const message: string[] = []
 
-      for (const keychain of keychains) {
-        message.push(`<b>${keychain.name}</b>: ${keychain.pattern}\n`)
+        message.push(`<a href="https://csfloat.com/item/${data.id}">${data.item.market_hash_name}</a>\n\n`)
+
+        for (const keychain of keychains) {
+          message.push(`<b>${keychain.name}</b>: ${keychain.pattern}\n`)
+        }
+
+        message.push(`\n`)
+        message.push(`<b>Price</b>: $${currentPrice}\n`)
+        message.push(`<b>Predicted price</b>: $${predictedPrice.toFixed(2)}\n`)
+        message.push(`<b>Profit</b>: ~$${profit.toFixed(2)}\n\n`)
+
+        await sendMessage(message.join(''))
       }
-
-      message.push(`\n`)
-      message.push(`<b>Price</b>: $${currentPrice}\n`)
-      message.push(`<b>Base price</b>: $${basePrice.toFixed(2)}\n`)
-      message.push(`<b>Keychains total</b>: $${keychainTotal.toFixed(2)}\n\n`)
-
-      await sendMessage(message.join(''))
     }
 
     CASHED_LISTINGS.add(data.id)

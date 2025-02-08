4a5354be4765692f49a4541cd37288106263d7eb
Eugene <eleonkov@gmail.com>
2025-02-08 09:08:37 +0100

    update

--- src/csfloat/csfloat-feed-checker.ts
+++ src/csfloat/csfloat-feed-checker.ts
@@ -11,15 +11,15 @@ import {
 } from '../api/csfloat'
 import { differenceInHours, format } from 'date-fns'
 import { toZonedTime } from 'date-fns-tz'
-import { median, sleep } from '../utils'
+import { sleep } from '../utils'
 import { CSFloatMarketHashNameHistory, CSFloatPlacedOrder } from '../types'
 import { sendMessage } from '../api/telegram'
 import path from 'path'
 import { readFileSync, writeFileSync } from 'fs'
 
-const blacklistedMarketOrders = new Set<string>()
 const blacklistedMarketListings = new Set<string>()
 const activeMarketOrders = new Map<string, CSFloatPlacedOrder>()
+const baseItemPriceCache = new Map<string, number>()
 const marketHistoryCache = new Map<string, CSFloatMarketHashNameHistory[]>()
 const pathname = path.join(__dirname, '../../float.json')
 
@@ -70,48 +70,34 @@ const floatFeedChecker = async () => {
     for (const item of response.data) {
       const now = format(new Date(), 'HH:mm:ss')
       const market_hash_name = item.item.market_hash_name
-      const activeMarketOrder = activeMarketOrders.get(market_hash_name)
       const floatBlackList: string[] = JSON.parse(readFileSync(pathname, 'utf8'))
-
-      if (floatBlackList.includes(market_hash_name)) continue
-      if (blacklistedMarketOrders.has(market_hash_name)) continue
-      if (blacklistedMarketListings.has(item.id)) continue
-      if (blackList.includes(market_hash_name)) continue
-
       const itemQuantity = item.reference.quantity
       const baseItemPrice = item.reference.base_price / 100
 
-      if (baseItemPrice < 9 || itemQuantity < 30) continue
+      if (activeMarketOrders.get(market_hash_name)) {
+        baseItemPriceCache.set(market_hash_name, baseItemPrice)
+      }
+
+      if (
+        baseItemPrice < 9 ||
+        activeMarketOrders.get(market_hash_name) ||
+        floatBlackList.includes(market_hash_name) ||
+        blacklistedMarketListings.has(item.id) ||
+        blackList.includes(market_hash_name) ||
+        itemQuantity < 30
+      ) {
+        continue
+      }
 
       const marketHistoryResponse = await getMarketHistory({ market_hash_name })
-      const medianPrice = median(marketHistoryResponse.map((item) => item.price / 100))
       const sales48h = marketHistoryResponse.filter((item) => {
         return differenceInHours(new Date(), toZonedTime(item.sold_at, 'Europe/Warsaw')) < 24 * 2
       })
 
-      if (sales48h.length < 10 || medianPrice < 9) {
+      if (sales48h.length < 10) {
         const floatBlackList: string[] = JSON.parse(readFileSync(pathname, 'utf8'))
         writeFileSync(pathname, JSON.stringify([...floatBlackList, market_hash_name], null, 4))
-      }
-
-      if (sales48h.length < 10 || medianPrice < 9) await sleep(10_000)
-      if (sales48h.length < 10 || medianPrice < 9) blacklistedMarketOrders.add(market_hash_name)
-      if (sales48h.length < 10 || medianPrice < 9) continue
-
-      if (activeMarketOrder) {
-        const listingReferenceId = marketHistoryResponse.sort((a, b) => a.price - b.price)[0].id
-        const resentOrders = await getBuyOrders({ id: listingReferenceId })
-        const simpleOrders = resentOrders.filter((i) => !!i.market_hash_name)
-        if (simpleOrders[0].market_hash_name === market_hash_name && simpleOrders[0].price > activeMarketOrder.price) {
-          await removeBuyOrder({ id: activeMarketOrder.id })
-          activeMarketOrders.delete(market_hash_name)
-          console.log(
-            `Order ${market_hash_name} has been removed. Prev price: ${activeMarketOrder.price}, new price: ${simpleOrders[0].price}`
-          )
-        }
-
         await sleep(10_000)
-
         continue
       }
 
@@ -147,7 +133,41 @@ const floatFeedChecker = async () => {
     }
   }
 
-  await sleep(60_000)
+  await sleep(60_000 * 10)
+
+  console.log(format(new Date(), 'HH:mm:ss'), 'Checking orders')
+
+  for (const [market_hash_name, order] of activeMarketOrders) {
+    const historyCache = marketHistoryCache.get(market_hash_name)
+    const baseItemPrice = baseItemPriceCache.get(market_hash_name)
+
+    if (historyCache && baseItemPrice) {
+      const listingReferenceId = historyCache.sort((a, b) => a.price - b.price)[0].id
+      const resentOrders = await getBuyOrders({ id: listingReferenceId })
+      const simpleOrders = resentOrders.filter((i) => !!i.market_hash_name)
+
+      const lowestOrderPrice = Number((simpleOrders[0].price / 100).toFixed(2))
+      const estimatedProfit = Number((((baseItemPrice - lowestOrderPrice) / lowestOrderPrice) * 100).toFixed(2))
+      const maxOrderPrice = Math.round((lowestOrderPrice + 0.01) * 100)
+
+      await sleep(5_000)
+
+      if (simpleOrders[0].price > order.price) {
+        await removeBuyOrder({ id: order.id })
+
+        if (estimatedProfit >= 7) {
+          await postBuyOrder({ market_hash_name, max_price: maxOrderPrice }).then(() => sleep(5_000))
+          await sendMessage(
+            `<b>[CSFLOAT ORDER]</b> <a href="https://csfloat.com/search?market_hash_name=${market_hash_name}&sort_by=lowest_price&type=buy_now">${market_hash_name}</a> $${(order.price / 100).toFixed(2)} -> $${(maxOrderPrice / 100).toFixed(2)}`
+          )
+        }
+      }
+
+      await sleep(5_000)
+    }
+  }
+
+  await sleep(5_000)
 
   floatFeedChecker()
 }

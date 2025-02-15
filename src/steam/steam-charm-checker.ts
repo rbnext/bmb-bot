0cdb90c819dd5c75306f42b1a33b9c0db8d3c659
Eugene <eleonkov@gmail.com>
2025-02-15 13:18:19 +0100

    update

--- src/steam/steam-charm-checker.ts
+++ src/steam/steam-charm-checker.ts
@@ -15,15 +15,15 @@ const CASHED_LISTINGS = new Set<string>()
 const configList = [
   {
     market_hash_name: 'Charm | Die-cast AK',
-    isSweet: (pattern: number, price: number) => {
+    isSweet: (pattern: number) => {
       if (
-        (pattern >= 1 && pattern <= 5000 && price <= 15) ||
-        (pattern > 5000 && pattern <= 9000 && price <= 10) ||
-        (pattern >= 20000 && pattern <= 23000 && price <= 17) ||
-        (pattern > 23000 && pattern <= 25000 && price <= 10) ||
-        (pattern >= 90000 && pattern <= 94999 && price <= 8) ||
-        (pattern >= 95000 && pattern <= 98999 && price <= 12) ||
-        (pattern >= 99000 && pattern <= 99999 && price <= 20)
+        (pattern >= 1 && pattern <= 5000) ||
+        (pattern > 5000 && pattern <= 9000) ||
+        (pattern >= 20000 && pattern <= 23000) ||
+        (pattern > 23000 && pattern <= 25000) ||
+        (pattern >= 90000 && pattern <= 94999) ||
+        (pattern >= 95000 && pattern <= 98999) ||
+        (pattern >= 99000 && pattern <= 99999)
       ) {
         return true
       }
@@ -34,8 +34,8 @@ const configList = [
   },
   {
     market_hash_name: "Charm | That's Bananas",
-    isSweet: (pattern: number, price: number) => {
-      if ((pattern >= 95000 || pattern <= 5000) && price <= 2.5) {
+    isSweet: (pattern: number) => {
+      if (pattern >= 96000 || pattern <= 4000) {
         return true
       }
 
@@ -70,17 +70,17 @@ const init = async () => {
         const steamMarketResponse = mapSteamMarketRenderResponse(response)
 
         for (const item of steamMarketResponse) {
-          if (!item.price || !item.pattern || CASHED_LISTINGS.has(item.listingId)) continue
+          if (!item.pattern || CASHED_LISTINGS.has(item.listingId)) continue
 
           const now = format(new Date(), 'HH:mm:ss')
           console.log(now, market_hash_name, item.pattern, item.price, config.start + item.position)
 
-          if (config.isSweet(item.pattern, item.price)) {
+          if (config.isSweet(item.pattern)) {
             const message: string[] = []
             message.push(
               `<a href="${getSteamUrl(market_hash_name, [])}">${market_hash_name}</a> | #${config.start + item.position}\n\n`
             )
-            message.push(`<b>Steam price</b>: $${item.price}\n`)
+            message.push(`<b>Steam price</b>: $${item.price ? item.price : 'Sold!'}\n`)
             message.push(`<b>Charm template</b>: #${item.pattern}\n`)
             await sendMessage(message.join(''))
           }

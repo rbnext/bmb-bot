443239e1e19967347bd6e64c0eaa4d15a6a8e5a4
Eugene <eleonkov@gmail.com>
2025-02-17 14:39:29 +0100

    update

--- src/csfloat/csfloat-checker.ts
+++ src/csfloat/csfloat-checker.ts
@@ -11,10 +11,6 @@ const CASHED_LISTINGS = new Set<string>()
 const MIN_PRICE = 500
 const MAX_PRICE = 10000
 
-// const isSweetFloat = (floatValue: number) => {
-//   return floatValue < 0.02 || (floatValue >= 0.07 && floatValue < 0.08) || (floatValue >= 0.15 && floatValue < 0.16)
-// }
-
 const handler = async () => {
   const response = await getCSFloatListings({
     sort_by: 'most_recent',
@@ -34,9 +30,6 @@ const handler = async () => {
     const isSouvenir = data.item.is_souvenir
     const market_hash_name = data.item.market_hash_name
 
-    const charm = data.item.keychains?.[0]
-    const charmPrice = charm?.reference?.price || 0
-
     const stickers = data.item.stickers || []
     const hasCombo = stickers.length === 4 && stickers.every(({ stickerId }) => stickerId === stickers[0].stickerId)
     const stickerTotal = stickers.reduce((acc, { reference }) => acc + (reference?.price || 0), 0)
@@ -44,11 +37,11 @@ const handler = async () => {
 
     const overpayment = Number((((currentPrice - predictedPrice) / predictedPrice) * 100).toFixed(2))
 
-    if (isSouvenir || overpayment > 20 || quantity <= 100 || totalTrades >= 100 || hasBadWear) {
+    if (isSouvenir || overpayment > 10 || quantity < 15 || totalTrades >= 100 || hasBadWear) {
       continue
     }
 
-    if (!(charmPrice > 50 || stickerTotal >= (hasCombo ? 1500 : 3000))) {
+    if (!(stickerTotal >= (hasCombo ? 1000 : 3000))) {
       continue
     }
 
@@ -58,27 +51,16 @@ const handler = async () => {
     const maxListingPrice = Math.max(...filteredListings.map((i) => i.price))
     const minListingPrice = Math.min(...filteredListings.map((i) => i.price))
 
-    console.log(format(new Date(), 'HH:mm:ss'), {
-      market_hash_name,
-      listingMedianPrice,
-      currentPrice,
-      stickerTotal,
-      charmPrice,
-      floatValue,
-    })
+    const now = format(new Date(), 'HH:mm:ss')
+    console.log(now, market_hash_name, currentPrice / 100, stickerTotal, hasCombo)
 
     if ((maxListingPrice / minListingPrice - 1) * 100 < 10) {
-      const estimatedToBeSold = listingMedianPrice + stickerTotal * (hasCombo ? 0.1 : 0.05) + charmPrice - 33
+      const estimatedToBeSold = listingMedianPrice + stickerTotal * (hasCombo ? 0.07 : 0.04)
       const estimatedProfitPercent = (estimatedToBeSold / currentPrice - 1) * 100
-
-      const floatLink = `https://csfloat.com/search?market_hash_name=${market_hash_name}&sort_by=lowest_price&type=buy_now`
 
       if (estimatedProfitPercent >= 5) {
         const message: string[] = []
-        message.push(`<a href="${floatLink}">${market_hash_name}</a>\n\n`)
-        if (charm) {
-          message.push(`<b>${charm.name}</b> ($${charmPrice / 100}) #${charm.pattern}\n\n`)
-        }
+        message.push(`<a href="https://csfloat.com/item/${data.id}">${market_hash_name}</a>\n\n`)
         for (const sticker of stickers) {
           message.push(
             `<b>${sticker.name}</b> ($${(sticker.reference?.price || 0) / 100}) ${sticker?.wear ? 'N/A' : '100%'}\n`

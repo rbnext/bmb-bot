4fbe299ed26af4c3ff211481aa86cd9dd5330871
Eugene <eleonkov@gmail.com>
2025-02-18 09:53:59 +0100

    update

--- src/helpers/executeBuffBargainTrade.ts
+++ src/helpers/executeBuffBargainTrade.ts
@@ -8,12 +8,11 @@ import {
   postGoodsBuy,
 } from '../api/buff'
 import { MarketGoodsItem, MessageType, Source } from '../types'
-import { generateMessage, getCSFloatItemPrice, getItemExterior, isLessThanXMinutes, median, sleep } from '../utils'
+import { generateMessage, getItemExterior, isLessThanXMinutes, median, sleep } from '../utils'
 import { sendMessage } from '../api/telegram'
 import { differenceInDays } from 'date-fns'
 import { GOODS_SALES_THRESHOLD, STEAM_PURCHASE_THRESHOLD } from '../config'
 import { getMaxPricesForXDays } from './getMaxPricesForXDays'
-import { getCSFloatListings } from '../api/csfloat'
 
 type BargainNotification = {
   sell_order_id: string
@@ -33,7 +32,7 @@ export const executeBuffBargainTrade = async (
   const goods_id = item.id
   const current_price = Number(item.sell_min_price)
 
-  const { isMinimalWear, isFieldTested, isStatTrak } = getItemExterior(item.market_hash_name)
+  // const { isMinimalWear, isFieldTested, isStatTrak } = getItemExterior(item.market_hash_name)
 
   const history = await getMarketGoodsBillOrder({ goods_id })
 
@@ -123,30 +122,30 @@ export const executeBuffBargainTrade = async (
       Number(lowestPricedItem.price) > bargain_price &&
       Number(lowestPricedItem.lowest_bargain_price) < bargain_price
     ) {
-      if ((isMinimalWear && Number(paintwear) < 0.08) || (isFieldTested && Number(paintwear) < 0.17)) {
-        const response = await getCSFloatListings({
-          market_hash_name: item.market_hash_name,
-          ...(isMinimalWear && { min_float: 0.07, max_float: 0.08 }),
-          ...(isFieldTested && { min_float: 0.15, max_float: 0.17 }),
-        })
+      // if ((isMinimalWear && Number(paintwear) < 0.08) || (isFieldTested && Number(paintwear) < 0.17)) {
+      //   const response = await getCSFloatListings({
+      //     market_hash_name: item.market_hash_name,
+      //     ...(isMinimalWear && { min_float: 0.07, max_float: 0.08 }),
+      //     ...(isFieldTested && { min_float: 0.15, max_float: 0.17 }),
+      //   })
 
-        const cs_float_price = getCSFloatItemPrice(response)
-        const estimated_profit = ((cs_float_price - current_price) / current_price) * 100
+      //   const cs_float_price = getCSFloatItemPrice(response)
+      //   const estimated_profit = ((cs_float_price - current_price) / current_price) * 100
 
-        if (estimated_profit >= 15) {
-          const response = await postGoodsBuy({ price: current_price, sell_order_id: lowestPricedItem.id })
+      //   if (estimated_profit >= 15) {
+      //     const response = await postGoodsBuy({ price: current_price, sell_order_id: lowestPricedItem.id })
 
-          if (response.code !== 'OK') {
-            console.log('Error:', JSON.stringify(response))
+      //     if (response.code !== 'OK') {
+      //       console.log('Error:', JSON.stringify(response))
 
-            return
-          }
+      //       return
+      //     }
 
-          sendMessage(generateMessage({ ...payload, type: MessageType.Purchased, csFloatPrice: cs_float_price }))
+      //     sendMessage(generateMessage({ ...payload, type: MessageType.Purchased, csFloatPrice: cs_float_price }))
 
-          return
-        }
-      }
+      //     return
+      //   }
+      // }
 
       const response = await postCreateBargain({
         price: bargain_price,

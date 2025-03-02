eb47446b0a9b49a2d33544c1af8459a548e0a404
Eugene <eleonkov@gmail.com>
2025-03-02 19:23:12 +0100

    update

--- src/steam/steam-market-checker.ts
+++ src/steam/steam-market-checker.ts
@@ -23,11 +23,11 @@ const marketSearchHandler = async (config: { start: number; count: number; proxy
     const now = format(new Date(), 'HH:mm:ss')
     const market_hash_name = item.marketHashName
 
-    if (market_hash_name in GOODS_CACHE && GOODS_CACHE[market_hash_name].price > item.sellPrice) {
-      console.log(`${now} ${market_hash_name} ${GOODS_CACHE[market_hash_name].price / 100} -> ${item.sellPrice / 100}`)
+    if (market_hash_name in GOODS_CACHE && GOODS_CACHE[market_hash_name].listings < item.sellListings) {
+      console.log(`${now} ${market_hash_name} ${GOODS_CACHE[market_hash_name].listings} -> ${item.sellListings}`)
     }
 
-    if (market_hash_name in GOODS_CACHE && GOODS_CACHE[market_hash_name].price > item.sellPrice) {
+    if (market_hash_name in GOODS_CACHE && GOODS_CACHE[market_hash_name].listings < item.sellListings) {
       try {
         const steamMarketResponse: MapSteamMarketRenderResponse[] = await getVercelMarketRender({
           market_hash_name,

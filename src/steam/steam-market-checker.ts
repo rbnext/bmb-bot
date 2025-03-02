e1ea330fe332890d4e94de515abe7e9d651cda6a
Eugene <eleonkov@gmail.com>
2025-03-02 22:45:29 +0100

    update

--- src/steam/steam-market-checker.ts
+++ src/steam/steam-market-checker.ts
@@ -29,7 +29,8 @@ const marketSearchHandler = async (config: { start: number; count: number; proxy
       item.sellListings >= 500 ||
       market_hash_name.includes('CZ75-Auto') ||
       (market_hash_name.includes('AK-47 | Slate') && !market_hash_name.includes('StatTrak')) ||
-      (market_hash_name.includes('AK-47 | Ice Coaled') && !market_hash_name.includes('StatTrak'))
+      (market_hash_name.includes('AK-47 | Ice Coaled') && !market_hash_name.includes('StatTrak')) ||
+      (market_hash_name.includes('Legion of Anubis') && !market_hash_name.includes('StatTrak'))
     ) {
       continue
     }

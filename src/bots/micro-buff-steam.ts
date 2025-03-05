5b37f25c04f287e1f55727e39743d24d0300e83c
Eugene <eleonkov@gmail.com>
2025-03-05 14:18:56 +0100

    update

--- src/bots/micro-buff-steam.ts
+++ src/bots/micro-buff-steam.ts
@@ -3,11 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import { getMarketGoods } from '../api/buff'
-import { isLessThanThreshold, sleep } from '../utils'
+import { generateMessage, isLessThanThreshold, sleep } from '../utils'
 import { format } from 'date-fns'
 import { sendMessage } from '../api/telegram'
-import { Source } from '../types'
-import { executeBuffToCSFloatTrade } from '../helpers/executeBuffToCSFloatTrade'
+import { MessageType, Source } from '../types'
 
 export const GOODS_CACHE: Record<number, { price: number }> = {}
 export const GOODS_BLACKLIST_CACHE: number[] = [30431, 30235, 30259, 30269, 30350]
@@ -27,30 +26,46 @@ const microBuffSteam = async () => {
 
         for (const item of marketGoods.data.items) {
           const now = format(new Date(), 'HH:mm:ss')
-          const current_price = Number(item.sell_min_price)
+          const currentPrice = Number(item.sell_min_price)
+          const steamPrice = Number(item.goods_info.steam_price)
+
+          const steamPriceAfterFee = Math.max(steamPrice - 0.01 - steamPrice * 0.15, steamPrice - 0.01 - 0.02)
+          const profitPercentage = ((steamPriceAfterFee - currentPrice) / currentPrice) * 100
 
           if (GOODS_BLACKLIST_CACHE.includes(item.id) || item.is_charm) {
             continue
           }
 
-          if (item.id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[item.id].price, current_price, 0.01)) {
-            GOODS_CACHE[item.id].price = current_price
+          if (item.id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[item.id].price, currentPrice, 0.01)) {
+            GOODS_CACHE[item.id].price = currentPrice
 
             continue
           }
 
           if (item.id in GOODS_CACHE) {
-            console.log(`${now}: ${item.market_hash_name} $${GOODS_CACHE[item.id].price} -> $${current_price}`)
+            console.log(`${now}: ${item.market_hash_name} $${GOODS_CACHE[item.id].price} -> $${currentPrice}`)
+            console.log(`|__ Profit: ${profitPercentage.toFixed(2)}%`)
           }
 
-          if (item.id in GOODS_CACHE && GOODS_CACHE[item.id].price > current_price) {
-            executeBuffToCSFloatTrade(item, { source: Source.BUFF_CSFLOAT })
+          if (item.id in GOODS_CACHE && GOODS_CACHE[item.id].price > currentPrice && profitPercentage > 150) {
+            await sendMessage(
+              generateMessage({
+                id: item.id,
+                price: currentPrice,
+                name: item.market_hash_name,
+                source: Source.BUFF_STEAM,
+                type: MessageType.Review,
+                estimatedProfit: profitPercentage,
+                medianPrice: steamPrice,
+              })
+            )
           }
 
-          GOODS_CACHE[item.id] = { price: current_price }
+          GOODS_CACHE[item.id] = { price: currentPrice }
         }
 
         if (marketGoods.data.items.length !== 50) {
+          console.log(`No more items to check. Page: ${page_num}`)
           break
         }
 

b89f1d628b265cde0b1b6218d7e1284a7af818cd
Eugene <eleonkov@gmail.com>
2025-03-06 22:54:19 +0100

    update

--- src/csfloat/csfloat-feed-checker.v3.ts
+++ src/csfloat/csfloat-feed-checker.v3.ts
@@ -81,7 +81,7 @@ const floatFeedChecker = async () => {
 
       const estimatedMedianProfit = Number(((listingMedianPrice - lowestOrderPrice) / lowestOrderPrice) * 100)
 
-      console.log(market_hash_name, estimatedMedianProfit.toFixed(2) + '%')
+      console.log(now, market_hash_name, estimatedMedianProfit.toFixed(2) + '%')
 
       if (lowestBuffPrice - 0.1 <= lowestOrderPrice / 100) {
         console.log(now, market_hash_name, 'Buff price is higher than CSFloat market price')

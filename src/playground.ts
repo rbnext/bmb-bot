f3dec747edb2477dda19f3ba4bd54e948b82c77f
Eugene <eleonkov@gmail.com>
2025-03-06 17:14:53 +0100

    update

--- src/playground.ts
+++ src/playground.ts
@@ -8,18 +8,25 @@ import { getMarketHashNameHistory } from './api/csfloat'
 import { differenceInMinutes } from 'date-fns'
 
 const init = async () => {
-  const response = await getMarketHashNameHistory({
-    market_hash_name: 'AK-47 | Redline (Field-Tested)',
-  })
+  const pages = Array.from({ length: 60 }, (_, i) => i + 1)
+  const pathname = path.join(__dirname, '../top-float-items.json')
 
-  const filteredResponse = response.filter((item) => {
-    const floatValue = Number(item.item.float_value.toFixed(2))
-    const stickerTotal = (item.item.stickers ?? []).reduce((acc, current) => acc + current.reference.price / 100, 0)
+  for (const page_num of pages) {
+    const goods = await getMarketGoods({
+      page_num,
+      min_price: 4,
+      max_price: 50,
+    })
+    const mostPopularItems: Record<string, number> = JSON.parse(readFileSync(pathname, 'utf8'))
 
-    return stickerTotal <= 5 && floatValue !== 0.15 && floatValue !== 0.16 && floatValue !== 0.17
-  })
-
-  console.log(filteredResponse.map((item) => item.price / 100))
+    for (const item of goods.data.items) {
+      if (item.market_hash_name in mostPopularItems) {
+        mostPopularItems[item.market_hash_name] = item.id
+      }
+    }
+    writeFileSync(pathname, JSON.stringify(mostPopularItems, null, 4))
+    await sleep(5_000)
+  }
 }
 
 init()

700185d5146e4a51a64a75219402fc1f329941b6
Eugene <eleonkov@gmail.com>
2025-03-09 12:21:43 +0100

    update

---
+++ src/csfloat/csfloat-sticker-checker.ts
@@ -0,0 +1,71 @@
+import 'dotenv/config'
+
+import schedule from 'node-schedule'
+import { sendMessage } from '../api/telegram'
+import { format } from 'date-fns'
+import { getCSFloatListings } from '../api/csfloat'
+import axios from 'axios'
+import { CSFloatListingItem } from '../types'
+
+const CASHED_LISTINGS = new Set<string>()
+
+const getStickerPercentage = (item: CSFloatListingItem, price: number) => {
+  const stickers = item.item.stickers || []
+  const predictedPrice = item.reference.predicted_price
+  const stickerTotal = stickers.reduce((acc, { reference }) => acc + (reference?.price || 0), 0)
+
+  return price >= predictedPrice && stickerTotal != 0 ? ((price - predictedPrice) / stickerTotal) * 100 : 0
+}
+
+const handler = async () => {
+  const response = await getCSFloatListings({
+    sort_by: 'most_recent',
+    min_price: 100,
+    max_price: 1000,
+    max_float: 0.6,
+  })
+
+  for (const data of response.data) {
+    if (CASHED_LISTINGS.has(data.id) || data.item.is_souvenir) continue
+
+    const price = data.price
+    const predictedPrice = data.reference.predicted_price
+    const market_hash_name = data.item.market_hash_name
+
+    const stickers = data.item.stickers || []
+    const stickerTotal = stickers.reduce((acc, { reference, wear }) => {
+      return acc + typeof wear === 'number' ? 0 : reference?.price || 0
+    }, 0)
+
+    const overpayment = Number((((price - predictedPrice) / predictedPrice) * 100).toFixed(2))
+
+    const SP = getStickerPercentage(data, price)
+
+    const now = format(new Date(), 'HH:mm:ss')
+    console.log(now, market_hash_name, overpayment, SP)
+
+    if (stickerTotal > 1000 && SP < 5) {
+      const message: string[] = []
+      message.push(`🤝 <b>[STICKER CHECKER]</b>` + ' ')
+      message.push(`<a href="https://csfloat.com/item/${data.id}">${market_hash_name}</a>\n\n`)
+
+      for (const sticker of stickers) {
+        message.push(`<b>${sticker.name}</b>: $${((sticker.reference?.price ?? 0) / 100).toFixed(2)}\n`)
+      }
+
+      await sendMessage(message.join(''), undefined, process.env.TELEGRAM_CSFLOAT_CHAT_ID)
+    }
+
+    CASHED_LISTINGS.add(data.id)
+  }
+}
+
+schedule.scheduleJob(`${process.env.SPEC} * * * * *`, async () => {
+  handler().catch((error) => {
+    const errorMessage = axios.isAxiosError(error) ? error.response?.data?.message : error.message
+
+    sendMessage(`Sticker checker error: ${errorMessage}`).then(() => {
+      process.exit(1)
+    })
+  })
+})

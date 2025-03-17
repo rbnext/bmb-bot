58f07a371db13d865d5d4d4884e7c4097af6452a
Eugene <eleonkov@gmail.com>
2025-03-17 21:19:19 +0100

    update

--- src/bots/buff-buff.ts
+++ src/bots/buff-buff.ts
@@ -13,49 +13,44 @@ export const GOODS_CACHE: Record<number, { price: number }> = {}
 export const GOODS_BLACKLIST_CACHE: number[] = [30431, 30235, 30259, 30269, 30350]
 
 const buffSteam = async () => {
-  const pages = Array.from({ length: 15 }, (_, i) => i + 1)
+  try {
+    const marketGoods = await getMarketGoods({
+      min_price: Number(process.env.MIN_BARGAIN_PRICE),
+      max_price: Number(process.env.MAX_BARGAIN_PRICE),
+    })
 
-  for (const page_num of pages) {
-    try {
-      const marketGoods = await getMarketGoods({
-        min_price: Number(process.env.MIN_BARGAIN_PRICE),
-        max_price: Number(process.env.MAX_BARGAIN_PRICE),
-        page_num,
-      })
+    for (const item of marketGoods.data.items) {
+      const current_price = Number(item.sell_min_price)
 
-      for (const item of marketGoods.data.items) {
-        const current_price = Number(item.sell_min_price)
-
-        if (GOODS_BLACKLIST_CACHE.includes(item.id) || item.is_charm) {
-          continue
-        }
-
-        if (item.id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[item.id].price, current_price, 0.1)) {
-          GOODS_CACHE[item.id].price = current_price
-
-          continue
-        }
-
-        if (item.id in GOODS_CACHE && GOODS_CACHE[item.id].price > current_price) {
-          executeBuffToBuffTrade(item, { source: Source.BUFF_BUFF, csFloatEnabled: true, pageNum: page_num })
-        }
-
-        GOODS_CACHE[item.id] = { price: current_price }
+      if (GOODS_BLACKLIST_CACHE.includes(item.id) || item.is_charm) {
+        continue
       }
 
-      await sleep(2_500)
-    } catch (error) {
-      console.log('Something went wrong', error)
+      if (item.id in GOODS_CACHE && isLessThanThreshold(GOODS_CACHE[item.id].price, current_price, 0.1)) {
+        GOODS_CACHE[item.id].price = current_price
 
-      if (error.message !== 'Request failed with status code 503') {
-        await sendMessage({ text: error?.message ?? 'Something went wrong.' })
-
-        return
+        continue
       }
 
-      await sendMessage({ text: `${error.message}. Restarting in 60 seconds...` })
-      await sleep(60_000)
+      if (item.id in GOODS_CACHE && GOODS_CACHE[item.id].price > current_price) {
+        executeBuffToBuffTrade(item, { source: Source.BUFF_BUFF, csFloatEnabled: true })
+      }
+
+      GOODS_CACHE[item.id] = { price: current_price }
     }
+
+    await sleep(2_500)
+  } catch (error) {
+    console.log('Something went wrong', error)
+
+    if (error.message !== 'Request failed with status code 503') {
+      await sendMessage({ text: error?.message ?? 'Something went wrong.' })
+
+      return
+    }
+
+    await sendMessage({ text: `${error.message}. Restarting in 60 seconds...` })
+    await sleep(60_000)
   }
 
   buffSteam()

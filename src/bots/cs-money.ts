ee9ef1f6f9803a59405e7d8996c085267b2bb8cd
Eugene <eleonkov@gmail.com>
2025-03-18 17:05:00 +0100

    cs-money

--- src/bots/cs-money.ts
+++ src/bots/cs-money.ts
@@ -4,15 +4,20 @@ import { format } from 'date-fns'
 import { getCSMoneyListings } from '../api/cs'
 import { isLessThanThreshold, sleep } from '../utils'
 import { CSMoneyItem } from '../types'
+import { getMaxPricesForXDays } from '../helpers/getMaxPricesForXDays'
 
 const GOODS_CACHE: Record<string, { price: number }> = {}
 
 const csMoneyTrade = async (item: CSMoneyItem) => {
   const name = item.asset.names.full
-
   const currentPrice = Number(item.pricing.basePrice)
 
-  console.log(`Buying ${name} for $${currentPrice}`)
+  const prices = await getMaxPricesForXDays(name)
+
+  const medianPrice = prices.length !== 0 ? Math.min(...prices) : 0
+  const estimatedProfit = ((medianPrice - currentPrice) / currentPrice) * 100
+
+  console.log(`${name} - $${currentPrice} - ${estimatedProfit.toFixed(2)}%`)
 }
 
 const csMoney = async () => {

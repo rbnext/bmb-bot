22f2c38bab4e749f1bda4150d0092919712681ee
Eugene <eleonkov@gmail.com>
2025-03-20 15:31:04 +0100

    test

--- src/playground.ts
+++ src/playground.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
 import { sleep } from './utils'
-import { getCSMoneyListings } from './api/cs'
+import { csMoneyAddToCart, getCSMoneyListings } from './api/cs'
 import { getBuyOrders, getCSFloatListings } from './api/csFloat'
 import { sendMessage } from './api/telegram'
 import { format } from 'date-fns'
@@ -12,44 +12,51 @@ const CASHED_LISTINGS = new Set<number>()
 const CS_FLOAT_PRICES: Record<string, number> = {}
 
 const init = async () => {
-  const response = await getCSMoneyListings({ limit: 60, offset: 0, minPrice: 5, maxPrice: 40 })
-
-  for (const item of response.items) {
-    const now = format(new Date(), 'HH:mm:ss')
-    if (CASHED_LISTINGS.has(item.asset.id)) {
-      continue
-    }
-    console.log(now, item.asset.names.full, item.pricing.basePrice)
-
-    if (CS_FLOAT_PRICES[item.asset.names.full] && CS_FLOAT_PRICES[item.asset.names.full] > item.pricing.basePrice) {
-      await sendMessage({ text: `${item.asset.names.full} - $${item.pricing.basePrice}` })
-    }
-
-    CASHED_LISTINGS.add(item.asset.id)
-  }
-  await sleep(2_000)
-
-  init()
+  const csMoneyPayload = { items: [{ id: String(36287812), price: 0.02 }] }
+  await csMoneyAddToCart(csMoneyPayload)
 }
 
-;(async () => {
-  const itemsIdsPath = path.join(__dirname, '../items-ids.json')
-  const itemsIds: Record<string, string> = JSON.parse(readFileSync(itemsIdsPath, 'utf8'))
+init()
 
-  for (const name in itemsIds) {
-    const response = await getBuyOrders({ id: itemsIds[name] })
-    const simpleOrders = response.filter((i) => !!i.market_hash_name)
+// const init = async () => {
+//   const response = await getCSMoneyListings({ limit: 60, offset: 0, minPrice: 5, maxPrice: 40 })
 
-    if (simpleOrders.length === 0) {
-      continue
-    }
+//   for (const item of response.items) {
+//     const now = format(new Date(), 'HH:mm:ss')
+//     if (CASHED_LISTINGS.has(item.asset.id)) {
+//       continue
+//     }
+//     console.log(now, item.asset.names.full, item.pricing.basePrice)
 
-    CS_FLOAT_PRICES[name] = Number((simpleOrders[0].price / 100).toFixed(2))
+//     if (CS_FLOAT_PRICES[item.asset.names.full] && CS_FLOAT_PRICES[item.asset.names.full] > item.pricing.basePrice) {
+//       await sendMessage({ text: `${item.asset.names.full} - $${item.pricing.basePrice}` })
+//     }
 
-    console.log(name, CS_FLOAT_PRICES[name])
+//     CASHED_LISTINGS.add(item.asset.id)
+//   }
+//   await sleep(2_000)
 
-    await sleep(3_000)
-  }
+//   init()
+// }
 
-  init()
-})()
+// ;(async () => {
+//   const itemsIdsPath = path.join(__dirname, '../items-ids.json')
+//   const itemsIds: Record<string, string> = JSON.parse(readFileSync(itemsIdsPath, 'utf8'))
+
+//   for (const name in itemsIds) {
+//     const response = await getBuyOrders({ id: itemsIds[name] })
+//     const simpleOrders = response.filter((i) => !!i.market_hash_name)
+
+//     if (simpleOrders.length === 0) {
+//       continue
+//     }
+
+//     CS_FLOAT_PRICES[name] = Number((simpleOrders[0].price / 100).toFixed(2))
+
+//     console.log(name, CS_FLOAT_PRICES[name])
+
+//     await sleep(3_000)
+//   }
+
+//   init()
+// })()

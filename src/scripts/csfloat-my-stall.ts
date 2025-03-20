d23fb7a7bfa83947f57738f6a0cd37e5ba0c6b45
Eugene <eleonkov@gmail.com>
2025-03-20 14:50:38 +0100

    my stall

--- src/scripts/csfloat-my-stall.ts
+++ src/scripts/csfloat-my-stall.ts
@@ -5,23 +5,23 @@ import { createListing, deleteListing, getMySellingList } from '../api/csfloat'
 import { isMoreThanXHours, sleep } from '../utils'
 import { format } from 'date-fns'
 
+const random = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
+
 const init = async () => {
   const listings = await getMySellingList({})
 
   for (const item of listings.data) {
     const now = format(new Date(), 'HH:mm:ss')
 
-    const stickers = item.item.stickers || []
-    const stickerTotal = stickers.reduce((acc, { reference }) => acc + (reference?.price || 0) / 100, 0)
-    const floatValue = item.item.float_value
-
     console.log(`[${now}] Processing: ${item.item.market_hash_name}`)
 
     if (isMoreThanXHours(item.created_at, 25)) {
       console.log(`├ Listing is older than 24 hours. Deleting and relisting...`)
 
-      await deleteListing({ id: item.id }).then(() => sleep(60_000))
-      await createListing({ asset_id: item.item.asset_id, price: item.price - 5 }).then(() => sleep(60_000))
+      await deleteListing({ id: item.id })
+      await sleep(random(50_000, 100_000))
+      await createListing({ asset_id: item.item.asset_id, price: item.price - 5 })
+      await sleep(random(50_000, 100_000))
 
       console.log(`└ Listing has been relisted with a price of ${item.price - 5}`)
 

64c1e15e3e49693c44103ab590b54cb932e21d66
Eugene <eleonkov@gmail.com>
2025-03-22 11:23:31 +0100

    test

--- src/bots/cs-money.ts
+++ src/bots/cs-money.ts
@@ -127,7 +127,7 @@ const csMoney = async () => {
         market_hash_name === 'Glock-18 | Gold Toof (Minimal Wear)' &&
         item.asset.float > 0.07 &&
         item.asset.float < 0.08 &&
-        currentPrice < 33
+        currentPrice < 32
       ) {
         try {
           await csMoneyPurchase({ items: [{ id: String(item.id), price: currentPrice }] })
